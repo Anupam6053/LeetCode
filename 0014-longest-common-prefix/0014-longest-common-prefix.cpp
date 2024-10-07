bdb0f69b88cc6a7fa2f76d699332e53d778754a5
Anupam6053 <157048783+Anupam6053@users.noreply.github.com>
2024-10-07 12:11:00 +0530

    Time: 3 ms (69.84%), Space: 11.1 MB (75.42%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -3,15 +3,15 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         
         for (int i = 0; i < strs[0].size(); i++) {
-            char s = strs[0][i];  // Character to check in each string
-            for (int j = 1; j < strs.size(); j++) {  // Start from 1 since strs[0] is the reference
+            char s = strs[0][i];  
+            for (int j =0; j < strs.size(); j++) { 
                 if (i >= strs[j].size() || strs[j][i] != s) {
-                    return strs[0].substr(0, i);  // Return common prefix up to index 'i'
+                    return strs[0].substr(0, i); 
                 }
             }
         }
 
-        // If no mismatch was found, return the entire first string
+        
         return strs[0];
     }
 };
